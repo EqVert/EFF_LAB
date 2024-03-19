e06919d5c200a4a80893f5f44c30057f2a1ef6b5
EqVert <EqVert@gmail.com>
2024-03-19 20:41:59 +0200

    Lab2

--- Lab_2/output_1.py
+++ Lab_2/output_1.py
@@ -1,18 +1,25 @@
-def calculate_score(name, age, gender, height, weight, score1, score2, score3, score4, score5):
-    # Бізнес-логіка для розрахунку загального рейтингу
-    total_score = score1 + score2 + score3 + score4 + score5
+class Person:
+    def __init__(self, name, age, gender, height, weight, scores):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.height = height
+        self.weight = weight
+        self.scores = scores
 
-    # Перевірка чи користувач є повнолітнім
-    is_adult = True if age >= 18 else False
+    def calculate_score(self):
+        total_score = sum(self.scores)
+        is_adult = True if self.age >= 18 else False
 
-    # Виведення результатів
-    print("Name:", name)
-    print("Age:", age)
-    print("Gender:", gender)
-    print("Height:", height)
-    print("Weight:", weight)
-    print("Total Score:", total_score)
-    print("Adult:", is_adult)
+        # Виведення результатів
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Gender:", self.gender)
+        print("Height:", self.height)
+        print("Weight:", self.weight)
+        print("Total Score:", total_score)
+        print("Adult:", is_adult)
 
-# Приклад виклику функції
-calculate_score("John", 25, "Male", 175, 70, 85, 90, 75, 88, 92)
+# Приклад виклику методу
+person = Person("John", 25, "Male", 175, 70, [85, 90, 75, 88, 92])
+person.calculate_score()
